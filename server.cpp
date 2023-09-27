66801dcb175416141852dce8e8a90a083767f6ff
sonjoyp <sonjoy.k.paul@gmail.com>
2023-09-27 00:44:38 -0400

    Introduce new function in the Server class

--- server.cpp
+++ server.cpp
@@ -18,6 +18,7 @@ class Server
 public:
 	Server();
 	~Server();
+	void readAndStoreInitMessagesFromFile();
 	void run();
 
 private:
@@ -28,8 +29,8 @@ private:
 	int len;
 	int s;
 	int new_s;
-	int i = 0;
-	int itotal = 0;
+	int iCurrentDisplayedMeesageIndex = 0;
+	int iTotalNumberofMessages = 0;
 	string messages[20];
 	string quit = "quit";
 	string msgget = "msgget";
@@ -46,38 +47,20 @@ private:
 	string loginAcc3 = "login david david01";
 	string loginAcc4 = "login mary mary01";
 	string temp;
-	ifstream ifile;
 	ofstream ofile;
 
 	void substring(char s[], char sub[], int p, int l);
-	// void handleShutdown();
-	// void handleQuit();
-	// void handleMsgGet();
-	// void handleLogout();
-	// void handleLogin();
-	// void handleMsgStore();
+
+	bool handleShutdown();
+	void handleQuit();
+	void handleMsgGet();
+	void handleLogout();
+	void handleLogin();
+	void handleMsgStore();
 };
 
-Server::Server() : islogin(false), isRlogin(false), shutdown(false), itotal(0)
+Server::Server() : islogin(false), isRlogin(false), shutdown(false), iTotalNumberofMessages(0)
 {
-	// File read and store messages from
-	ifile.open("messages.txt");
-	int j = 0;
-	if (ifile.is_open())
-	{
-		getline(ifile, messages[j]);
-		messages[j] += '\n';
-		messages[j] += '\0';
-		while (ifile)
-		{
-			++j;
-			++itotal;
-			getline(ifile, messages[j]);
-			messages[j] += '\n';
-			messages[j] += '\0';
-		}
-	}
-	ifile.close();
 
 	// File write and store messages to
 
@@ -126,6 +109,47 @@ void Server::substring(char s[], char sub[], int p, int l)
 	sub[c] = '\0';
 }
 
+void Server::readAndStoreInitMessagesFromFile()
+{
+	// File read and store messages from
+	ifstream ifile;
+	ifile.open("messages.txt");
+	int cnt = 0;
+	if (ifile.is_open())
+	{
+		getline(ifile, messages[cnt]);
+		messages[cnt] += '\n';
+		messages[cnt] += '\0';
+		while (ifile)
+		{
+			++cnt;
+			++iTotalNumberofMessages;
+			getline(ifile, messages[cnt]);
+			messages[cnt] += '\n';
+			messages[cnt] += '\0';
+		}
+	}
+	ifile.close();
+}
+
+bool Server::handleShutdown()
+{
+	if (isRlogin)
+	{
+		shutdown = true;
+		temp = "Response from Server: 200 OK\n";
+		strcpy(buf, temp.c_str());
+		send(new_s, buf, strlen(buf) + 1, 0);
+		return true;
+	}
+	else
+	{
+		temp = "Response from Server: 402 User not allowed\n";
+		strcpy(buf, temp.c_str());
+		return false;
+	}
+}
+
 // void Server::handleShutdown() {
 //     if (isRlogin) {
 //         shutdown = true;
@@ -136,53 +160,94 @@ void Server::substring(char s[], char sub[], int p, int l)
 //     }
 // }
 
-// void Server::handleQuit() {
-//     strcpy(buf, "Response from Server: 200 OK\n");
-// }
+void Server::handleQuit()
+{
+	temp = "Response from Server: 200 OK\n";
+	strcpy(buf, temp.c_str());
+	send(new_s, buf, strlen(buf) + 1, 0);
+}
 
-// void Server::handleMsgGet() {
-//     buf = "Response from Server: 200 OK\n\t\t " + messages[0] + "\n";
-//     strcpy(buf, temp.c_str());
-// }
+void Server::handleMsgGet()
+{
+	temp = "Response from Server: 200 OK\n\t\t ";
+	temp += messages[iCurrentDisplayedMeesageIndex];
+	temp += "\n";
+	strcpy(buf, temp.c_str());
+	iCurrentDisplayedMeesageIndex++;
+	if (iCurrentDisplayedMeesageIndex == iTotalNumberofMessages)
+		iCurrentDisplayedMeesageIndex = 0;
+}
 
-// void Server::handleLogout() {
-//     buf = "Response from Server: No users logged in\n";
-//     if (isRlogin) {
-//         buf = "Response from Server: 200 OK\n";
-//         isRlogin = false;
-//     }
-//     if (islogin) {
-//         buf = "Response from Server: 200 OK\n";
-//         islogin = false;
-//     }
-// }
+void Server::handleLogout()
+{
+	temp = "Response from Server: No users logged in\n";
+	if (isRlogin)
+	{
+		temp = "Response from Server: 200 OK\n";
+		isRlogin = false;
+	}
+	if (islogin)
+	{
+		temp = "Response from Server: 200 OK\n";
+		islogin = false;
+	}
+	strcpy(buf, temp.c_str());
+	iCurrentDisplayedMeesageIndex++;
+}
 
-// void Server::handleLogin() {
-//     buf = "Response from Server: 200 OK\n";
-//     islogin = true;
+void Server::handleLogin()
+{
+	if (strcmp(buf, loginAcc2.c_str()) == 10 ||
+		strcmp(buf, loginAcc3.c_str()) == 10 ||
+		strcmp(buf, loginAcc4.c_str()) == 10)
+	{
+		printf("In first if\n");
+		temp = "Response from Server: 200 OK\n";
+		islogin = true;
+		strcpy(buf, temp.c_str());
+	}
+	else if (strcmp(buf, loginAcc1.c_str()) == 10)
+	{
+		printf("In second if\n");
+		temp = "Response from Server: 200 OK\n";
+		isRlogin = true;
+		strcpy(buf, temp.c_str());
+	}
+	else  // The ASCII value of the space character (' ') is 32 in decimal notation.
+	{
+		temp = "Response from Server: 410 Wrong UserID or Password\n";
+		strcpy(buf, temp.c_str());
+	}
+}
 
-//     strcpy(buf, temp.c_str());
-// }
-
-// void Server::handleMsgStore() {
-//     char tempb[MAX_LINE];
-//     substring(buf, tempb, 10, strlen(buf) - 10);
-//     if (islogin || isRlogin) {
-//         if (itotal < 20) {
-//             string temp(tempb);
-//             messages[itotal] = temp;
-//             ++itotal;
-//             temp = '\n' + tempb;
-//             ofile.write(temp.c_str(), strlen(tempb) + 1);
-//             temp = "Response from Server: 200 OK\n";
-//             strcpy(buf, temp.c_str());
-//         } else {
-//             strcpy(buf, "Response from Server: 402 No more space max limit exceed\n");
-//         }
-//     } else {
-//         strcpy(buf, "Response from Server: 401 You are not currently logged in, login first\n");
-//     }
-// }
+void Server::handleMsgStore()
+{
+	substring(buf, tempb, 10, strlen(buf) - 10);
+	if (islogin || isRlogin)
+	{
+		if (iTotalNumberofMessages < 20)
+		{
+			temp = tempb;
+			messages[iTotalNumberofMessages] = temp;
+			++iTotalNumberofMessages;
+			temp = '\n';
+			temp += tempb;
+			ofile.write(temp.c_str(), strlen(tempb) + 1);
+			temp = "Response from Server: 200 OK\n";
+			strcpy(buf, temp.c_str());
+		}
+		else
+		{
+			temp = "Response from Server: 402 No more space max limit exceed\n";
+			strcpy(buf, temp.c_str());
+		}
+	}
+	else
+	{
+		temp = "Response from Server: 401 You are not currently logged in, login first\n";
+		strcpy(buf, temp.c_str());
+	}
+}
 
 void Server::run()
 {
@@ -203,111 +268,40 @@ void Server::run()
 			// Description: It allows the user to shutdown the user
 			if (strcmp(buf, stdown.c_str()) == 10) // The ASCII value of the newline character (\n) is 10 in decimal notation and 0x0A in hexadecimal notation.
 			{
-				if (isRlogin)
-				{
-					shutdown = true;
-					temp = "Response from Server: 200 OK\n";
-					strcpy(buf, temp.c_str());
-					send(new_s, buf, strlen(buf) + 1, 0);
+				if (handleShutdown())
 					break;
-				}
-				else
-				{
-					temp = "Response from Server: 402 User not allowed\n";
-					strcpy(buf, temp.c_str());
-				}
 			}
 			// Quit Command
 			// Description: It allows the user to exit out of the server
 			if (strcmp(buf, quit.c_str()) == 10)
 			{
-				temp = "Response from Server: 200 OK\n";
-				strcpy(buf, temp.c_str());
-				send(new_s, buf, strlen(buf) + 1, 0);
+				handleQuit();
 				break;
 			}
 			// MSGGET Command
 			if (strcmp(buf, msgget.c_str()) == 10)
 			{
-				temp = "Response from Server: 200 OK\n\t\t ";
-				temp += messages[i];
-				temp += "\n";
-				strcpy(buf, temp.c_str());
-				i++;
-				if (i == itotal)
-					i = 0;
+				handleMsgGet();
 			}
 			// Logout Method
 			// It will allow the user to logout.
 			if (strcmp(buf, logout.c_str()) == 10)
 			{
-				temp = "Response from Server: No users logged in\n";
-				if (isRlogin)
-				{
-					temp = "Response from Server: 200 OK\n";
-					isRlogin = false;
-				}
-				if (islogin)
-				{
-					temp = "Response from Server: 200 OK\n";
-					islogin = false;
-				}
-				strcpy(buf, temp.c_str());
-				i++;
+				handleLogout();
 			}
 
-			// cout << "*" << strcmp(buf, login.c_str()) << endl;
 			// Login Method
 			// Allow the user to login
-			if (strcmp(buf, loginAcc2.c_str()) == 10 ||
-				strcmp(buf, loginAcc3.c_str()) == 10 ||
-				strcmp(buf, loginAcc4.c_str()) == 10)
+			if (strcmp(buf, login.c_str()) == 32)
 			{
-				temp = "Response from Server: 200 OK\n";
-				islogin = true;
-				strcpy(buf, temp.c_str());
+				handleLogin();
 			}
-			else if (strcmp(buf, loginAcc1.c_str()) == 10)
-			{
-				temp = "Response from Server: 200 OK\n";
-				isRlogin = true;
-				strcpy(buf, temp.c_str());
-			}
-			else if (strcmp(buf, login.c_str()) == 32) // The ASCII value of the space character (' ') is 32 in decimal notation.
-			{
-				temp = "Response from Server: 410 Wrong UserID or Password\n";
-				strcpy(buf, temp.c_str());
-			}
+
 			// MSGStore
 			// Once the user is logged in, it will allow them to send one message to the server
-
 			if (strcmp(buf, msgstore.c_str()) == 32)
 			{
-				substring(buf, tempb, 10, strlen(buf) - 10);
-				if (islogin || isRlogin)
-				{
-					if (itotal < 20)
-					{
-						temp = tempb;
-						messages[itotal] = temp;
-						++itotal;
-						temp = '\n';
-						temp += tempb;
-						ofile.write(temp.c_str(), strlen(tempb) + 1);
-						temp = "Response from Server: 200 OK\n";
-						strcpy(buf, temp.c_str());
-					}
-					else
-					{
-						temp = "Response from Server: 402 No more space max limit exceed\n";
-						strcpy(buf, temp.c_str());
-					}
-				}
-				else
-				{
-					temp = "Response from Server: 401 You are not currently logged in, login first\n";
-					strcpy(buf, temp.c_str());
-				}
+				handleMsgStore();
 			}
 
 			send(new_s, buf, strlen(buf) + 1, 0);
@@ -322,6 +316,7 @@ void Server::run()
 int main()
 {
 	Server server;
+	server.readAndStoreInitMessagesFromFile();
 	server.run();
 	return 0;
 }
